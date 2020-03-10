1cd81ef41a8f751d7d56702050c42198bd7ffb1d
minung.han <hmu332233@gmail.com>
2020-03-11 02:33:42 +0900

    refactor simple counter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 };
 const countStore = createStore(countModifier, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
@@ -19,8 +24,8 @@ const onChange = () => {
 }
 countStore.subscribe(onChange);
 
-const handleAdd = () => countStore.dispatch({ type: 'ADD' });
-const handleMinus = () => countStore.dispatch({ type: 'MINUS' });
+const handleAdd = () => countStore.dispatch({ type: ADD });
+const handleMinus = () => countStore.dispatch({ type: MINUS });
 
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
