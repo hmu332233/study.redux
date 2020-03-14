8c783f3626543f2064d1ba78613a3bea70bffcd9
minung.han <hmu332233@gmail.com>
2020-03-15 05:42:19 +0900

    add simple todo list

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+
+const createToDo = toDo => {
+  const li = document.createElement('li');
+  li.innerText = toDo;
+  ul.appendChild(li);
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = ''
+  createToDo(toDo);
+}
+
+form.addEventListener('submit', onSubmit);
