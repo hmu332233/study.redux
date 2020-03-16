3119360200934b1f343c1a809614f315c6380904
minung.han <hmu332233@gmail.com>
2020-03-17 01:44:21 +0900

    add react todo

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { createStore } from 'redux';
+
+const ADD = 'ADD';
+const DELETE = 'DELETE';
+
+// action creators
+const addToDo = text => {
+  return {
+    type: ADD,
+    text,
+  }
+}
+const deleteToDo = id => {
+  return {
+    type: DELETE,
+    id,
+  }
+}
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter(item => item.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+export default store;
